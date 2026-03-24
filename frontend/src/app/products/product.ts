6e3d5ce80b9c1848e07494db69e597668c04a55f
Mit Patel <akypatel2010@gmail.com>
2026-03-24 21:14:36 +0530

    fix: remove submodules and add folders properly

---
+++ frontend/src/app/products/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  id: number;
+  name: string;
+  desc: string;
+  status: string;
+  date: Date;
+  category: string;
+  sku: number;
+  price: number;
+  stock: number;
+  dis_per: number;
+  dis_price: number;
+}
